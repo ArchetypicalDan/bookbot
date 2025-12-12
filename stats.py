202d94040f0b9e69c67dca564cc78389868b38bd
ArchetypicalDan <dan.hurley@gmail.com>
2025-12-11 21:39:28 -0500

    added report_counts function for chars

--- stats.py
+++ stats.py
@@ -13,5 +13,14 @@ def count_chars(text_content):
             char_counts[cl] = 1
     
     return char_counts
-        
 
+def sort_on(items):
+    return items["num"]
+        
+def report_counts(char_counts):
+    dict_list = []
+    for item in char_counts:
+        dict_list.append({"char": item, "num": char_counts[item]})
+
+    return sorted(dict_list, reverse=True, key=sort_on)
+    
