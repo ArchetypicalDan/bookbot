89a09a861e97eb0a7cc35a6e9a991d4a8481c186
ArchetypicalDan <dan.hurley@gmail.com>
2025-12-11 20:57:30 -0500

    updated main.py to read a file and count the words

---
+++ main.py
@@ -0,0 +1,14 @@
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+    
+def count_words(content):
+    words = content.split()
+    return len(words)
+
+def main():
+    book_text = get_book_text("./books/frankenstein.txt")
+    num_words = count_words(book_text)
+    print(f"Found {num_words} total words")
+
+main()
