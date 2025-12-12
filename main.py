687f8fdf0d089d537e1e3b6a4a8d0b8bffff05df
ArchetypicalDan <dan.hurley@gmail.com>
2025-12-11 21:39:05 -0500

    added print_report function

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-from stats import count_words, count_chars
+from stats import *
+
+
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
     
+def print_report(filepath, num_words, char_report):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in char_report:
+        if(item["char"].isalpha()):
+            print(f"{item["char"]}: {item["num"]}")
+    print("============= END ===============")
+
+    
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
+    filepath = "./books/frankenstein.txt"
+    book_text = get_book_text(filepath)
     num_words = count_words(book_text)
     char_count = count_chars(book_text)
-    print(f"Found {num_words} total words")
-    print(char_count)
+    char_report = report_counts(char_count)
+    print_report(filepath, num_words, char_report)
+    
 
 main()
